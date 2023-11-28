6cd3577258698994b7f55fee21c172653a27df9f
thiagoxfx <thiagoxfx@gmail.com>
2023-11-27 21:37:28 -0300

    correção de problemas flake8

--- myfunctions.py
+++ myfunctions.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -12,13 +12,17 @@ CHROOMEDRIVER_EXEC = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
 CHROME_BINARY_PATH = ROOT_FOLDER / 'chrome-win32' / 'chrome.exe'
 
 # Function to read dataset from excel
+
+
 def load_excel_data(file_path):
     workbook = load_workbook(file_path)
     worksheet = workbook['webservices']
     return [row[0].value for row in worksheet.iter_rows(min_row=2, min_col=2,
-        max_col=2) if row[0].value is not None]
+            max_col=2) if row[0].value is not None]
 
 # Function to open Chrome
+
+
 def make_chrome_browser(*options: str) -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
 
@@ -42,6 +46,8 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
     return chrome_browser
 
 # Function to open tabs in Chrome
+
+
 def open_webservices(chrome_browser, *list_links):
     for i, n in enumerate(list_links):
         try:
